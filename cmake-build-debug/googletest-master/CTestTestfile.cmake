bf2a8b8b1f8bf85633c1cde14930e805a7fdbe2a
qymaensheel <bartosz@nieroda.pl>
2020-02-11 15:07:13 +0100

    delete compilator data

--- cmake-build-debug/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/bnier/Google Drive/AGH/S3/Informatyka/googletest-master
-# Build directory: C:/Users/bnier/Google Drive/AGH/S3/Informatyka/netsim/cmake-build-debug/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/bnier/Google Drive/AGH/S3/Informatyka/googletest-master/googlemock
-# Build directory: C:/Users/bnier/Google Drive/AGH/S3/Informatyka/netsim/cmake-build-debug/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
